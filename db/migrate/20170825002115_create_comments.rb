cee4091cfc1280ac0bb9905f512fc3fe37f2f95d
bernulander <bernulander@gmail.com>
2017-08-24 19:29:34 -0500

    implemented polymorphic comments

--- db/migrate/20170825002115_create_comments.rb
+++ db/migrate/20170825002115_create_comments.rb
@@ -1,12 +1,13 @@
-class CreateMainComments < ActiveRecord::Migration[5.1]
+class CreateComments < ActiveRecord::Migration[5.1]
   def change
-    create_table :main_comments do |t|
+    create_table :comments do |t|
       t.text :body
+      t.integer :commentable_id
+      t.string :commentable_type
       t.string :image
       t.boolean :escorrupto
       t.integer :likes
       t.integer :dislikes
-      t.references :target, foreign_key: true
 
       t.timestamps
     end
--- spec/models/threaded_comment_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ThreadedComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
